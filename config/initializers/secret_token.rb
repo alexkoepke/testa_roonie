32ac9c97c9ba3d57f56ee3b27f8d0da17557bc6b
Prelang Builder <builder@prelang.com>
2014-03-27 22:06:25 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestaRoonie::Application.config.secret_key_base = '0e1bc7cffea9b251adc82598904dcbca3ffc4b18a6cb48bda388ece1e433ae614e648187f4e528100d842f672eeaf7a40fb24da94c77b3baf15fea9fac163ca6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestaRoonie::Application.config.session_store :cookie_store, key: '_testa_roonie_session'
